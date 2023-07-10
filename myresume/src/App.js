42c82de54e1f24a94d908bfec07b68ee82391478
YueGuo88 <109397943+YueGuo88@users.noreply.github.com>
2023-07-10 22:04:27 +0000

    submission

---
+++ myresume/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Resume from './Resume';
+import './Resume.css';
+
+const App = () => {
+  return (
+    <div className="Resume">
+      <Resume />
+    </div>
+  );
+};
+
+export default Resume;
+
+
---
+++ myresume/src/Resume.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+      <div>
+        <h1>Yue Guo</h1>
+        <h2>Contact Information</h2>
+        <ul>
+          <li>Email: carmenyuer@gmail.com</li>
+          <li>Phone: 123-456-7890</li>
+          <li>Website: carmenyuer.com</li>
+        </ul>
+  
+        <h2>Education</h2>
+        <ul>
+          <li>City University of Seattle, Master's degree of CS, 2024</li>
+          <li>University of Washington, Bachelor's degree in Architectural Design, 2017</li>
+        </ul>
+  
+        <h2>Skills</h2>
+        <ul>
+          <li>Programming Lauguages: C#, Python</li>
+          <li>Frameworks and Libraries: React, SpringBoot, Django, Node.js, Express.js</li>
+          <li>Database Systems: MySQL, SQLServer, MongDB</li>
+          <li>Operating Systems: Windows, Linux</li>
+        </ul>
+  
+        <h2>Work Experience</h2>
+        <ul>
+          <li>Never Want to Work</li>
+        </ul>
+  
+        <h2>Projects</h2>
+        <ul>
+          <li>Project 1: Wait for 100 years</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Resume;
